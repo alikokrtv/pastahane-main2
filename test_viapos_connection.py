47298df64f553e00a2852c247eb208ee63689c54
alikokrtv <alikokrtv@gmail.com>
2025-08-19 17:17:48 +0300

    feat: add command line args to override viapos database connection settings

--- test_viapos_connection.py
+++ test_viapos_connection.py
@@ -5,6 +5,7 @@ Viapos veritabanı bağlantısını test etmek için basit script
 import os
 import sys
 import django
+import argparse
 from pathlib import Path
 
 # Django ayarlarını yükle
@@ -14,7 +15,24 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bakery_management.settings')
 django.setup()
 
 from django.db import connections
+from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
+
+def apply_overrides(args):
+    """Argümanlarla gelen host/port/user/password değerlerini DATABASES['viapos'] üzerine uygula."""
+    dbs = connections.databases  # settings.DATABASES referansı
+    if 'viapos' not in dbs:
+        raise ImproperlyConfigured("'viapos' veritabanı konfigürasyonu bulunamadı.")
+    cfg = dbs['viapos']
+    if args.host:
+        cfg['HOST'] = args.host
+    if args.port:
+        cfg['PORT'] = str(args.port)
+    if args.user:
+        cfg['USER'] = args.user
+    if args.password:
+        cfg['PASSWORD'] = args.password
+
 
 def test_viapos_connection():
     """Viapos veritabanı bağlantısını test et"""
@@ -57,5 +75,15 @@ def test_viapos_connection():
         return False
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Viapos MySQL bağlantı testi")
+    parser.add_argument("--host", help="MySQL host")
+    parser.add_argument("--port", type=int, help="MySQL port")
+    parser.add_argument("--user", help="MySQL kullanıcı adı")
+    parser.add_argument("--password", help="MySQL parola")
+    args = parser.parse_args()
+
+    # Argümanlarla override uygula
+    apply_overrides(args)
+
     print("Viapos veritabanı bağlantısı test ediliyor...")
     test_viapos_connection()
